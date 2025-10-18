3c52a1d008219ba3413b8d273ade7386ae2387de
Blake Blackshear <blake@frigate.video>
2025-10-18 07:18:28 -0500

    add missing keyboard shortcut to dialog

--- src/components/ImageAnnotator.tsx
+++ src/components/ImageAnnotator.tsx
@@ -73,6 +73,7 @@ const ImageAnnotator = (props: IImageAnnotationProps) => {
     { codes: ["w"], action: "Add Box" },
     { codes: ["d"], action: "Mark Difficult" },
     { codes: ["s"], action: "Cycle Label" },
+    { codes: ["Shift", "s"], action: "Previous Label" },
     { codes: ["Tab"], action: "Select Next Box" },
     { codes: ["Del"], action: "Delete Box" },
     { codes: ["Esc"], action: "Deselect/Cancel" },
